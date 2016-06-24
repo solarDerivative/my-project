9344f44ba09d94db73c5b43608dd44f9cce88bf3
solarDerivative <solarderivative@gmail.com>
2016-06-24 16:16:02 -0500

    sandbox checklist
    AHEIEHAFH

--- events.js
+++ events.js
@@ -50,3 +50,34 @@ function picLink() {
 	}
 
 }
+
+//////////////////////////////////////////
+
+var checklist = document.getElementById("checklist");
+
+var items = checklist.querySelectorAll("li");
+var inputs = checklist.querySelectorAll("input");
+
+for (i = 0; i < items.length; i++) {
+	items[i].addEventListener("click", editItem);
+	inputs[i].addEventListener("blur", updateItem);
+	inputs[i].addEventListener("keypress", itemKeypress);
+}
+
+function editItem() {
+	this.className = "edit";
+	var input = this.querySelector("input");
+	input.focus();
+	input.setSelectionRange(0, input.value.length);
+}
+
+function updateItem() {
+	this.previousElementSibling.innerHTML = this.value;
+	this.parentNode.className = "";
+}
+
+function itemKeypress(event) {
+	if (event.which === 13) {
+		updateItem.call(this);
+	}
+}
