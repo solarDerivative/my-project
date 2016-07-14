e90b748e4c42ce47fe4d1ff5656ce7d8f67eebf1
solarDerivative <solarderivative@gmail.com>
2016-07-13 22:59:58 -0500

    CSS general revamp

---
+++ randcontent.js
@@ -0,0 +1,17 @@
+/*
+TODO
+	-Create an array of all elements (let's start with just four) with class name "revpanel"
+	^	-var heebleglorken = document.getAllElementsByClassName("revpanel");
+	
+	
+		-first, I should go make all panels hidden by default in css probably
+		-loop through the array of review panels.
+		-each time, choose one of the panels at random.
+			-Make a random number generator to produce a value between one and the length of the array
+		-At the index of (result of RNG), set the revpanel display to inline
+		-Remove element from array
+		-^ Complete this loop a total of three times
+
+	-for(i = 0; i < 3; i++)
+
+*/
