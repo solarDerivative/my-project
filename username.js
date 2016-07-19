64b22c374c2f697e9cc3479f47297b5bb658d925
solarDerivative <solarderivative@gmail.com>
2016-07-19 16:01:31 -0500

    7.19
    username.js refactoring WIP

--- username.js
+++ username.js
@@ -1,6 +1,58 @@
 window.onload = showUsername();
 window.onload = showPicture();
+window.onload = createReview();
 
+//objects
+//inputs - strings
+
+var inputName = document.getElementById("inputN");
+var inputImg = document.getElementById("inputI");
+var inputBody = document.getElementById("inputB");
+
+//outputs - arrays
+
+var outputName = document.getElementsByClassName("outputN");
+var outputImg = document.getElementsByClassName("outputI");
+var outputBody = document.getElementsByClassName("outputB");
+
+//object constructors
+
+function user(name, img){
+	//"methods" which call values
+	this.userName = name;
+	this.userImg = img;
+}
+
+function post(name, img, body){
+	//"methods" which point to values
+	this.postName = name;
+	this.postImg = img;
+	this.postBody = body;
+}
+
+//high-end, non-specific functions - need setDefaults (ACTUALLY that could go into user right?)
+
+function getAndStoreData(input, store){
+	//"methods" which call values
+	this.inputType = input;
+	this.storedDataInput = store;
+
+	if(typeof(Storage) !== "undefined"){
+		localStorage.setItem(store, input);
+	} else {
+		document.getElementsByClassName("result").innerHTML = "Sorry! No Web Storage support...";
+	}
+}
+
+function showData(output, store){
+	this.outputType = output;
+	this.storedDataOutput = store;
+
+	for(var i = 0; i < output.length; i++){
+		output[i].innerHTML = store;
+	}
+}
+/*
 function getUsername(){
 	var input1 = document.getElementById("nameInput").value;
 	if(typeof(Storage) !== "undefined"){
@@ -26,7 +78,6 @@ function showUsername(){
 	for(var i = 0; i < resultArray.length; i++){
 		resultArray[i].innerHTML = userName;
 	}
-	/*console.log(userName);*/
 }
 
 function getPicture(){
@@ -41,7 +92,6 @@ function getPicture(){
 function showPicture(){
 	var guest2 = "http://blog.algonquinstudios.com/wp-content/uploads/2014/04/anonymous.gif"
 	var userPic = document.getElementsByClassName("myImg");
-	console.log(userPic);
 	var pic = localStorage.getItem("picture");
 
 	if(pic == null){
@@ -52,3 +102,26 @@ function showPicture(){
 		userPic[i].src = pic;
 	}
 }
+
+function getReview(){
+	var input3 = document.getElementById("descInput");
+	if(typeof(Storage) !== "undefined"){
+		localStorage.setItem("description", input3);
+	} else {
+		document.getElementsByClassName("result").innerHTML = "Sorry! No Web Storage support..."
+	}
+}
+
+function createReview(){
+
+	var guest3 = "test description";
+	var otptB = document.getElementById("userB");
+	var otptH = document.getElementById("userH");
+	var otptP = document.getElementById("userP");
+	var userDesc = localStorage.getItem("description");
+
+	if(userDesc == null){
+		userDesc = guest3;
+	}
+	
+}*/
---
+++ userrev.js
@@ -0,0 +1,7 @@
+/*	userrev.js
+goal: retrieve username, profile image, and content
+format information and add it to the html
+
+first, make html template on sandbox with empty data for pic, name, and review
+next, create an input field for a review
+make sure sandbox review panel works*/
